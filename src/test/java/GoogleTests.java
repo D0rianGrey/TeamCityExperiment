62c2109ad47023d517e6ced56d93c045ea9ea042
Yevhenii Vakerin <yevhenii.vakerin@dataart.com>
2023-09-23 12:21:45 +0200

    Add new approach 3

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -1,17 +1,23 @@
-import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Browsers.CHROME;
-import static com.codeborne.selenide.Selenide.open;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class GoogleTests {
 
     @Test
-    void testGoogle() {
+    void testGoogle() throws MalformedURLException {
 ////        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "src/main/java/org/example/drivers/chromedriver");
 ////        WebDriverManager.chromedriver().s
-        Configuration.browser = CHROME;
-        open("https://www.google.com/");
+//        Configuration.browser = CHROME;
+//        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://chromedriver:4444/wd/hub"), chromeOptions);
+        driver.get("http://www.google.com");
+        driver.quit();
+//        open("https://www.google.com/");
 ////
 //        WebDriver driver = new ChromeDriver();
 //        driver.get("https://www.google.com/");
