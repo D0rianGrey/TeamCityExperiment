4411c1016b9cd8858e839035768c2fb0331eaf03
Yevhenii Vakerin <yevhenii.vakerin@dataart.com>
2023-09-23 18:59:18 +0200

    Add edit #1

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -7,8 +7,10 @@ public class GoogleTests extends BaseTest {
     void testGoogle() {
 
         var url = propertiesConfigurator.getFinalProperties().getProperty("url");
+        var env = propertiesConfigurator.getFinalProperties().getProperty("env");
 
         System.out.println(url);
+        System.out.println(env);
 
         open(url);
 
