9c4848a79308fa7a34d4474f0f64c90a7c5f1d86
Yevhenii Vakerin <yevhenii.vakerin@dataart.com>
2023-09-23 12:26:01 +0200

    Add new approach 4

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -1,26 +1,22 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class GoogleTests {
 
-    @Test
-    void testGoogle() throws MalformedURLException {
-////        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "src/main/java/org/example/drivers/chromedriver");
-////        WebDriverManager.chromedriver().s
-//        Configuration.browser = CHROME;
-//        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+    @BeforeTest
+    public void setUp() {
         ChromeOptions chromeOptions = new ChromeOptions();
-        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://chromedriver:4444/wd/hub"), chromeOptions);
-        driver.get("http://www.google.com");
-        driver.quit();
-//        open("https://www.google.com/");
-////
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://www.google.com/");
-//        driver.quit();
+        Configuration.remote = "http://chromedriver:4444/wd/hub";
+        Configuration.browserCapabilities = new DesiredCapabilities();
+        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+    }
+
+    @Test
+    void testGoogle() {
+        Selenide.open("http://www.google.com");
     }
 }
