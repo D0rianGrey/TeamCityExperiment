cc344c8a0fa63fd8e8f91868cad53f3d44e7049e
Yevhenii Vakerin <yevhenii.vakerin@dataart.com>
2023-09-25 18:59:07 +0200

    Add fix 10

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,19 +1,25 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.projectFeatures.buildReportTab
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 version = "2023.05"
 
 project {
     buildType(Build)
+//    features {
+//        buildReportTab {
+//            id = "PROJECT_EXT_52"
+//            title = "Test Results"
+//            startPage = "allure-report.zip!/allure-report/index.html"
+//        }
+//    }
     features {
-        buildReportTab {
-            id = "PROJECT_EXT_52"
-            title = "Test Results"
-            startPage = "allure-report.zip!/allure-report/index.html"
+        feature {
+            type = "allureReport"
+            param("allureRunnerMode", "runner")
+            param("allureReportGenerate", "true")
+            param("allureResultsPattern", "allure-results/**")
         }
     }
 }
@@ -25,14 +31,9 @@ object Build : BuildType({
     """
     publishArtifacts = PublishMode.ALWAYS
 
-    // Define common paths as parameters
     params {
-//        param("allureResultsPath", "%teamcity.build.checkoutDir%/allure-results")
-//        param("allureReportPath", "%teamcity.build.checkoutDir%/allure-report")
         param("env", "cloud_chrome")
         param("url", "https://www.google.com.ua/")
-//        param("teamcity.artifacts.prepublishingCommands", "move /Y %allureReportPath% allure-report")
-//        param("artifacts", "%allureReportPath% => allure-report.zip")
     }
 
     vcs {
@@ -40,32 +41,19 @@ object Build : BuildType({
     }
 
     steps {
+
         maven {
             name = "Test"
             goals = "clean test -Denv=%env% -Durl=%url%"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
-
-//        // Use the parameterized paths
-//        exec {
-//            name = "Generate Allure Report"
-//            path = "allure"
-//            arguments = "generate %allureResultsPath% -o %allureReportPath%"
+//
+//        script {
+//            scriptContent = """
+//                #!/bin/bash
+//                allure generate ./allure-results --clean -o ./allure-report
+//            """
 //        }
-//        step {
-//            name = "Generate test report"
-//            type = "allureReportGeneratorRunner"
-//            param("allure.result.directory", "allure-results")
-//            param("allure.report.path.prefix", "allure-report")
-//            executionMode = BuildStep.ExecutionMode.ALWAYS
-//        }
-
-        script {
-            scriptContent = """
-                #!/bin/bash
-                allure generate ./allure-results --clean -o ./allure-report
-            """
-        }
     }
 
     triggers {
