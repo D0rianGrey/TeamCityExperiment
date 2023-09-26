f55f54898d9b4332cc6ba6883de4670c053bcd47
Yevhenii Vakerin <yevhenii.vakerin@dataart.com>
2023-09-26 19:08:01 +0200

    Add fix 51

--- .teamcity/Build.kt
+++ .teamcity/Build.kt
@@ -35,15 +35,15 @@ object Build : BuildType({
         }
 
 
-        script {
-            name = "Send 'Hello' to Microsoft Teams"
-            executionMode = BuildStep.ExecutionMode.ALWAYS
-            val WEBHOOK_URL =
-                "https://vakerin.webhook.office.com/webhookb2/9c1222ef-4e94-4519-8587-4c6d274a897d@09e68569-5204-4f37-8857-099b0cdfc689/IncomingWebhook/e665721392a24e019db0c59371fe5bb2/a217d337-3a25-44ea-bf80-629df276aeca"
-            scriptContent = """
-        curl -H 'Content-Type: application/json' -d '{"text": "Hello"}' \$WEBHOOK_URL
-    """.trimIndent()
-        }
+//        script {
+//            name = "Send 'Hello' to Microsoft Teams"
+//            executionMode = BuildStep.ExecutionMode.ALWAYS
+//            val WEBHOOK_URL =
+//                "https://vakerin.webhook.office.com/webhookb2/9c1222ef-4e94-4519-8587-4c6d274a897d@09e68569-5204-4f37-8857-099b0cdfc689/IncomingWebhook/e665721392a24e019db0c59371fe5bb2/a217d337-3a25-44ea-bf80-629df276aeca"
+//            scriptContent = """
+//        curl -H 'Content-Type: application/json' -d '{"text": "Hello"}' \$WEBHOOK_URL
+//    """.trimIndent()
+//        }
 
         script {
             name = "Send 'Hello' to Microsoft Teams"
@@ -53,7 +53,7 @@ object Build : BuildType({
             val WEBHOOK_URL =
                 "https://vakerin.webhook.office.com/webhookb2/9c1222ef-4e94-4519-8587-4c6d274a897d@09e68569-5204-4f37-8857-099b0cdfc689/IncomingWebhook/e665721392a24e019db0c59371fe5bb2/a217d337-3a25-44ea-bf80-629df276aeca"
             scriptContent = """
-        curl -H 'Content-Type: application/json' -d '{"text": "$ALLURE_REPORT_URL"}' \$WEBHOOK_URL
+        curl -H 'Content-Type: application/json' -d '{"text": "[View Allure Report]($ALLURE_REPORT_URL)"}' $WEBHOOK_URL
     """.trimIndent()
         }
 
