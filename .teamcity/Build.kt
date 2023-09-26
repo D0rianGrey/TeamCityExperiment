2a867e825c2c7143d372ba9153e7e2584dde69e6
Yevhenii Vakerin <yevhenii.vakerin@dataart.com>
2023-09-26 17:04:35 +0200

    Add fix 40

--- .teamcity/Build.kt
+++ .teamcity/Build.kt
@@ -3,6 +3,7 @@ import jetbrains.buildServer.configs.kotlin.DslContext
 import jetbrains.buildServer.configs.kotlin.PublishMode
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 // Setting for Build
@@ -30,6 +31,37 @@ object Build : BuildType({
         maven {
             name = "Generate Allure Report from allure-results"
             goals = "allure:report"
+        }
+
+        script {
+            name = "Send Notification to Teams"
+            scriptContent = """
+            #!/bin/bash
+
+            WEBHOOK_URL="https://vakerin.webhook.office.com/webhookb2/9c1222ef-4e94-4519-8587-4c6d274a897d@09e68569-5204-4f37-8857-099b0cdfc689/IncomingWebhook/e665721392a24e019db0c59371fe5bb2/a217d337-3a25-44ea-bf80-629df276aeca"
+            BUILD_TYPE_ID="%teamcity.buildType.id%"
+            BUILD_ID="%teamcity.build.id%"
+            SERVER_URL="%teamcity.serverUrl%"
+            
+            REPORT_LINK="$SERVER_URL/buildConfiguration/$BUILD_TYPE_ID/$BUILD_ID?buildTab=report_project1_Test_Results"
+
+            PAYLOAD="{
+                \\"@type\\": \\"MessageCard\\",
+                \\"@context\\": \\"http://schema.org/extensions\\",
+                \\"summary\\": \\"New Allure Report\\",
+                \\"sections\\": [{
+                    \\"activityTitle\\": \\"New Allure Report is Ready\\",
+                    \\"activitySubtitle\\": \\"Click below to view the report\\",
+                    \\"activityImage\\": \\"\\",
+                    \\"facts\\": [{
+                        \\"name\\": \\"Report:\\",
+                        \\"value\\": \\"[View Report]($REPORT_LINK)\\"
+                    }]
+                }]
+            }"
+
+            curl -H "Content-Type: application/json" -d "$PAYLOAD" $WEBHOOK_URL
+        """
         }
     }
 
