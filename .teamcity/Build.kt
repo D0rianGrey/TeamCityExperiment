7a43cea422c61cab82559ffac9b6395bde4b5ef1
Yevhenii Vakerin <yevhenii.vakerin@dataart.com>
2023-09-26 20:20:30 +0200

    Add fix 64

--- .teamcity/Build.kt
+++ .teamcity/Build.kt
@@ -60,7 +60,7 @@ object Build : BuildType({
                     "contentType": "application/vnd.microsoft.card.adaptive",
                     "contentUrl": null,
                     "content": {
-                        "schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                        "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
                         "type": "AdaptiveCard",
                         "version": "1.2",
                         "body": [
