61fcbdf78b71bcd63f6e27c24022b9c45d25851a
Yevhenii Vakerin <yevhenii.vakerin@dataart.com>
2023-09-27 14:38:05 +0200

    Add fix 99

--- .teamcity/Build.kt
+++ .teamcity/Build.kt
@@ -46,7 +46,8 @@ object Build : BuildType({
             name = "Command Line from UI"
             executionMode = BuildStep.ExecutionMode.ALWAYS
             scriptContent = """
-                    echo "##teamcity[setParameter name='env.PASSED_TESTS' value='10']"
+                PASSED_COUNT=${'$'}(jq '.counters.passed' allure-report/export/prometheusData.txt)
+                echo "##teamcity[setParameter name='env.PASSED_TESTS' value='${'$'}PASSED_COUNT']"
                 """.trimIndent()
         }
 
