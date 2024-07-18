1effdf384fc535449f13cab9700f0b9486409921
Yevhenii Vakerin <yevhenii.vakerin@dataart.com>
2024-07-19 00:19:19 +0200

    Add commit with new version of teamcity #2

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.project
 import jetbrains.buildServer.configs.kotlin.version
 
-version = "2024.07"
+version = "2024.03"
 
 project(MainProject) // entry point for the project
